dbd1cb56a28ff37262bb8929e649b3db6db68cb2
Ethan Farrell <78605366+Trailblazer780@users.noreply.github.com>
2022-11-01 12:13:10 -0300

    Added functioning forms

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -26,4 +26,9 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
+
+  onEditItem(index:number){
+    this.slService.startedEditing.next(index);
+  }
+
 }
